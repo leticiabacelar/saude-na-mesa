c8947869233d305176f52c35a8fef9dc8eb04a09
leticiabacelar <141786949+leticiabacelar@users.noreply.github.com>
2023-08-11 20:36:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function carregar(){
+    var data = new Date();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+    var segundo = data.getSeconds();
+    msg.InnerHTML = 'agora são ${hora} hora, ${minutos} e ${segundo} segundo,'>
+}
